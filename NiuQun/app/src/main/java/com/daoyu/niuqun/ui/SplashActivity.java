cfc03e35e08e710869aac62be9626d7994a69f82
clude729 <clude729@163.com>
2018-10-07 16:41:26 +0800

    第一次提交项目

---
+++ NiuQun/app/src/main/java/com/daoyu/niuqun/ui/SplashActivity.java
@@ -0,0 +1,17 @@
+package com.daoyu.niuqun.ui;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import com.daoyu.niuqun.R;
+
+public class SplashActivity extends Activity
+{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+    }
+}
